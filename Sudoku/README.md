efca98cca638f2de9160481af6a0a4de2892773f
Liam M <twoutrigger@outlook.com>
2022-05-07 22:29:30 -0400

    add sudoku generator

--- Sudoku/README.md
+++ Sudoku/README.md
@@ -1,5 +1,9 @@
-gui.py + solver.py from https://www.techwithtim.net/tutorials/python-programming/sudoku-solver-backtracking/
+gui.py + solver.py from:
+https://www.techwithtim.net/tutorials/python-programming/sudoku-solver-backtracking/
 
 Using Sudoku to better understand the concept of entropy
 
 Additional methods of solving for Sudoku include Linear Programming and Wave Function Collapse, will give these a try in the future
+
+Original gui.py had sudoku board hard wired. Added sudoku generator script from:
+https://stackoverflow.com/questions/45471152/how-to-create-a-sudoku-puzzle-in-python
---
+++ Sudoku/sudoku_generator.py
@@ -0,0 +1,32 @@
+# sudoku_generator.py
+
+def generate_board():
+
+    base  = 3
+    side  = base*base
+
+    # pattern for a baseline valid solution
+    def pattern(r,c): return (base*(r%base)+r//base+c)%side
+
+    # randomize rows, columns and numbers (of valid base pattern)
+    from random import sample
+    def shuffle(s): return sample(s,len(s))
+    rBase = range(base)
+    rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ]
+    cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+    nums  = shuffle(range(1,base*base+1))
+
+    # produce board using randomized baseline pattern
+    board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+
+    # for line in board: print(line)
+
+    squares = side*side
+    empties = squares * 3//5
+    for p in sample(range(squares),empties):
+        board[p//side][p%side] = 0
+
+    numSize = len(str(side))
+    # for line in board: print("["+"  ".join(f"{n or '.':{numSize}}" for n in line)+"]")
+
+    return board
