a74b89c8079cdd1373697ad3cb5f9849f071f7e9
Liam M <twoutrigger@outlook.com>
2022-06-10 09:01:46 -0400

    got car to move; input ndarray

--- Reinforcement Learning/rl_race_track.py
+++ Reinforcement Learning/rl_race_track.py
@@ -5,6 +5,7 @@ env = gym.make("CarRacing-v1")
 
 print(env.action_space)
 print(env.spec.max_episode_steps)
+# print(type(env.action_space.sample()))
 
 EPISODES = 10 #25000
 
@@ -15,9 +16,11 @@ done = False
 while not done:
 
     # action = np.random.randint(0, 3)
-    new_state, reward, done, _ = env.step([0 for i in range(200)])
+    # new_state, reward, done, _ = env.step([4 for i in range(200)])
+    new_state, reward, done, _ = env.step(env.action_space.sample())
+    # print(new_state.shape)
+    # print(reward)
 
-    # env.step([0 for i in range(200)])
     env.render()
 
 env.close()
