8d47a4c58fdb185c5ae703ba5b1d2d62907d5d5d
Liam M <twoutrigger@outlook.com>
2023-03-12 12:53:12 -0400

    twilio receiving; ngrok 502 bad gateway

---
+++ Twilio/twilio_002_receive_sms/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+
+    resp = MessagingResponse()
+
+    resp.message("Testing response message.")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
